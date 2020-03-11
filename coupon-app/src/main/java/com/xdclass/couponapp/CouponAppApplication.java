3f1b5918c2c72630214403db647dafd818b2a297
lucky-dog-w <1069849274@qq.com>
2020-03-11 18:49:09 +0800

    我再试试提交 解决冲突

--- coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java
+++ coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("第一次修改");
+		System.out.println("第一次修改，我再提交就冲突了");
 		SpringApplication.run(CouponAppApplication.class, args);
 	}
 
