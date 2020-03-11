3fcd485ef43174b1cce90c0e5c8d295a20605d98
lucky-dog-w <1069849274@qq.com>
2020-03-11 18:56:51 +0800

    第5commit

---
+++ coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java
@@ -0,0 +1,18 @@
+package com.xdclass.couponapp;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.xdclass.couponapp.mapper")
+public class CouponAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("第一次修改，我再提交就冲突了aaa");
+		System.out.println("第一次修改，我再提交就冲突了bbb");
+		System.out.println("第一次修改,我在这修改会冲突");
+		SpringApplication.run(CouponAppApplication.class, args);
+	}
+
+}
