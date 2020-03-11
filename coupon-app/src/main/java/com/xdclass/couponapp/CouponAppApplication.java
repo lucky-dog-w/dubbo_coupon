b680df954c4cce37de915269e2ce9372b4366d7c
lucky-dog-w <1069849274@qq.com>
2020-03-11 18:43:31 +0800

    试试main修改提交第一次

--- coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java
+++ coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("第一次修改");
 		SpringApplication.run(CouponAppApplication.class, args);
 	}
 
