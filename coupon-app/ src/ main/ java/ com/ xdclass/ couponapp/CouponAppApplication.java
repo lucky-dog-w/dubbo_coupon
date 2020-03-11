94b7cfaafc917684fd3a7bf6977e5e4057b16ff3
lucky-dog-w <55725125+lucky-dog-w@users.noreply.github.com>
2020-03-11 20:24:11 +0800

    Update and rename coupon-app/src/main/java/com/xdclass/couponapp/CouponAppApplication.java to  coupon-app/ src/ main/ java/ com/ xdclass/ couponapp/CouponAppApplication.java

--- coupon-app/ src/ main/ java/ com/ xdclass/ couponapp/CouponAppApplication.java
+++ coupon-app/ src/ main/ java/ com/ xdclass/ couponapp/CouponAppApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("第一次修改dd，我再提交就冲突了bbddb1222");
-		System.out.println("第一次修改dddd,我在这修改会冲突1222");
+		System.out.println("第一次修改");
+		System.out.println("第一次修改,我在这修改会冲突1222");
 		SpringApplication.run(CouponAppApplication.class, args);
 	}
 
